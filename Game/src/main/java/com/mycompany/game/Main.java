0c42482cf8bcba96678f856d540f0f289e320d11
Manyu864 <abim50057@gmail.com>
2024-06-29 15:44:02 +0700

    Game Selesai

---
+++ Game/src/main/java/com/mycompany/game/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.game;
+
+/**
+ *
+ * @author Asus
+ */
+    public class Main {
+    
+        public static void main(String[] args) {
+            // Create a new game object
+            Game game = new Game();
+            
+            // Start the game
+            game.play();
+
+    }
+}
